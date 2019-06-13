565223410f0b3a49b87fbd1515d5b63d0989d662
Jim MacArthur <jim.macarthur@codethink.co.uk>
2019-06-13 12:18:43 +0100

    Remove more unnecessary things from _onResolveClick

--- loleaflet/src/layer/marker/Annotation.js
+++ loleaflet/src/layer/marker/Annotation.js
@@ -343,11 +343,6 @@ L.Annotation = L.Layer.extend({
 
 	_onResolveClick: function (e) {
 		L.DomEvent.stopPropagation(e);
-		this._data.reply = this._nodeReplyText.value;
-		// Assigning an empty string to .innerHTML property in some browsers will convert it to 'null'
-		// While in browsers like Chrome and Firefox, a null value is automatically converted to ''
-		// Better to assign '' here instead of null to keep the behavior same for all
-		this._checkBounds();
 		this._map.fire('AnnotationResolve', {annotation: this});
 	},
 
